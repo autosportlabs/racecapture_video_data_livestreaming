c5b3b3430a93316fda05baa501f1e4728da52b0e
ddimensia <ddimensia@gmail.com>
2023-02-12 10:02:15 -0800

    A little more cleanup
    Changes include:
    * Quoted all `echo` lines
    * Used a block to populate the .ratpoisonrc file
    * Added other groups likely to be needed, depending on configuration

--- racecapture_streaming_install.sh
+++ racecapture_streaming_install.sh
@@ -4,13 +4,16 @@ RC_APP_URL=`curl -s https://podium.live/software | grep -Po '(?<=<a href=")[^"]*
 RC_APP_FILENAME=`basename $RC_APP_URL`
 
 # install dependencies
-echo Installing additional packages
+echo "Installing additional packages"
 sudo apt-get -qq update
 sudo apt-get -y -qq install v4l-utils tk gstreamer1.0-plugins-bad gstreamer1.0-libav gconf2 gnome-shell-extensions ratpoison
 
-# enable access to RaceCapture USB connection
-echo Enabling access to USB
+# enable access to RaceCapture USB and other /dev files
+echo "Enabling access to system devices"
 sudo adduser $USER dialout >/dev/null 2>&1
+sudo adduser $USER render >/dev/null 2>&1
+sudo adduser $USER video >/dev/null 2>&1
+sudo adduser $USER input >/dev/null 2>&1
 sudo echo "ATTRS{idVendor}==16d0, ATTRS{idProduct}==07f1, MODE=0666" | sudo tee /etc/udev/rules.d/70-autosportlabs.racecapture.rules >/dev/null 2>&1
 
 # The commands below assume we're in $HOME so just change directories now
@@ -27,22 +30,22 @@ fi
 echo "Installing RC App '$RC_APP_FILENAME'"
 wget -q --show-progress -c "$RC_APP_URL" -O - | tar -xjp
 
-echo Download livestreaming scripts
+echo "Download livestreaming scripts"
 mkdir -p $HOME/streamer
 pushd .
 cd $HOME/streamer
 wget -q -O streamer.sh https://raw.githubusercontent.com/autosportlabs/racecapture_video_data_livestreaming/main/streamer.sh
 chmod +x streamer.sh
 popd
-echo Configuring auto-start for racecapture and streaming
-{
-        echo "xset s off"
-        echo "xset -dpms"
-        echo "xset s noblank"
-        echo "exec /bin/bash -c 'cd ~/racecapture && LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 ./race_capture >> ~/racecapture.log 2>&1'"
-        echo "exec $HOME/streamer/streamer.sh >> ~/streamer.log 2>&1"
-} > ~/.ratpoisonrc
 
-echo Changing your window manager to single app mode
+echo "Configuring auto-start for racecapture and streaming"
+cat > ~/.ratpoisonrc <<'EOF'
+exec gsettings set org.gnome.desktop.session idle-delay 0
+exec gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+exec gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+exec /bin/bash -c 'cd ~/racecapture && LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6 ./race_capture >> ~/racecapture.log 2>&1'
+exec $HOME/streamer/streamer.sh >> ~/streamer.log 2>&1
+EOF
+
+echo "Changing your window manager to single app mode"
 sudo sed -i "s/Session=.*/Session=ratpoison/g" /var/lib/AccountsService/users/$USER
-
